6963371a34260e59fd130ca59c2366032420422b
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-13 13:50:07 +0100

    Created router for page navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Soundscape web application
-      </h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Container fluid className={styles.Main}>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/signin" element={<h1>Sign in</h1>} />
+          <Route path="/signup" element={<h1>Sign up</h1>} />
+          <Route element={<h1>Page not found</h1>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
