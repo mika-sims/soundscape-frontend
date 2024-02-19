f6ba11d08120c0f6ad0bc8f9d44451e5d91ef380
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-19 17:07:59 +0100

    Edited landing page style

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import appStyles from "../App.module.css";
 import styles from "../styles/LandingPage.module.css";
 import logo from "../assets/images/logo.svg";
 import Button from "react-bootstrap/Button";
@@ -10,13 +11,12 @@ const LandingPage = () => {
   return (
     <div className={styles.LandingPage}>
       <Card className={styles.Card}>
-        <h1 className={`${styles.Title}`}>SOUNDSCAPE</h1>
+        <h1 className={`${appStyles.Header}`}>SOUNDSCAPE</h1>
         <Card.Img variant="top" src={logo} className={styles.Logo} />
         <Card.Body className="text-center">
-          <h4 className="mb-3">Your acoustic environment</h4>
-          <p>Share your acoustic environment</p>
+          <h4 className={appStyles.Header}>Share your acoustic environment</h4>
           <p>Get inspired by the sounds of the world</p>
-          <Button className={styles.Button}>
+          <Button className={appStyles.Button}>
             <Link to={"/signup"}>Get Started</Link>
           </Button>
         </Card.Body>
